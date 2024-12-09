38cf72465e2a667f02faf20c527fc2d01bd53c18
dungtaok <dungtaak@gmail.com>
2024-12-09 12:41:30 +0700

    update

--- src/utilz/LoadSave.java
+++ src/utilz/LoadSave.java
@@ -1,7 +1,5 @@
 package utilz;
 
-
-
 import javax.imageio.ImageIO;
 
 import java.awt.image.BufferedImage;
@@ -43,7 +41,7 @@ public class LoadSave {
     public static final String BOSS5_ATLAS = "boss5_atlas.png";
 
 
-    public static final String MONSTER_EYE1_ATLAS = "pate.png";
+    public static final String MONSTER_EYE1_ATLAS = "moneye1_atlas.png";
     public static final String BALL1EYE = "ball1Eye.png";
 
     public static final String MONSTER2_ATLAS = "monster2_atlas.png";
